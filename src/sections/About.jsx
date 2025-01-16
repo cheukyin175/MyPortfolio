cc3e0a709ccd4d2842d1a7ec9fb8192ba34cdc1f
Hugo Lau <cheukyin175@gmail.com>
2025-01-16 17:21:44 +0800

    added about me section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
                         
                         <div>
                             <p className="grid-headtext">Hi, I am Hugo</p>
-                            <p className="grid-subtext">I am currently a year 2 data science student studying in City University of Hong Kong, honing my skill in UI/UX design and data analysis, with a passion on computing and finance field</p>
+                            <p className="grid-subtext">I am currently a data science student studying in City University of Hong Kong, honing my skill in data science and exploring the path of being full stack developer, with a passion on computing and finance field</p>
                         </div>
                     </div>
                 </div>
@@ -19,7 +19,7 @@ const About = () => {
                         
                         <div>
                             <p className="grid-headtext">My Skills</p>
-                            <p className="grid-subtext">I am proficient in Python, SQL, and have experience in using Tableau, and UI/UX design tools such as Figma</p>
+                            <p className="grid-subtext">I am proficient in Python, SQL for doing data analysis and machine learning, and have experience in using Tableau, PowerBI experience.</p>
                         </div>
                     </div>
                 </div>
